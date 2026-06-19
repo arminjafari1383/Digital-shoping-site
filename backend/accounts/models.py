d25d793ac5336092673c1e16d7c7c32b77f6ff25
unknown <arminjafri138386@gmail.com>
2026-06-19 01:07:03 -0700

    feature:create model for user (optional:login with email)

---
+++ backend/accounts/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20,blank=True)
+    avatar = models.ImageField(
+        upload_to='avatars/',
+        blank=True,
+        null=True
+    )
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
+    
