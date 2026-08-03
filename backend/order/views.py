085637ee0b3b7437dd57a884cb52f5731f5aa46f
unknown <arminjafri138386@gmail.com>
2026-08-03 02:08:46 -0700

    dashboard admin master

--- backend/order/views.py
+++ backend/order/views.py
@@ -7,6 +7,8 @@ from .serializers import OrderSerializer
 from .models import Order, OrderItem
 from products.models import Product
 from accounts.models import User
+from django.utils import timezone
+
 
 
 class CheckoutView(APIView):
@@ -72,6 +74,33 @@ class DashboardView(APIView):
             .order_by("-sold")[:10]
         )
 
+        today = timezone.now().date()
+
+        today_orders = Order.objects.filter(
+            created_at__date = today
+        )
+
+        today_orders_count = today_orders.count()
+
+        today_income = (
+
+            today_orders.filter(status="paid")
+            .aggregate(total=Sum("total_price"))["total"] or 0
+        )
+
+        now = timezone.now()
+
+        month_orders = Order.objects.filter(
+            created_at__year = now.year,
+            created_at__month = now.month
+        )
+
+        month_orders_count = month_orders.count()
+
+        month_income = (
+            month_orders.filter(status="paid")
+            .aaggregate(total=Sum("total_price"))["total"] or 0
+        )
 
         return Response({
 
@@ -88,6 +117,16 @@ class DashboardView(APIView):
             "latest_orders": latest_orders,
 
             "best_selling_products": best_selling,
+
+            "today_orders": today_orders_count,
+
+            "today_income": today_income,
+
+            "month_orders": month_orders_count,
+
+            "month_income": month_income,
         })
+
+    
 
     
