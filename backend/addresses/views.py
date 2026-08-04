b845207d8973575e9da7273366f8b2e9fceb35a2
unknown <arminjafri138386@gmail.com>
2026-08-04 06:38:49 -0700

    edit prform_create

--- backend/addresses/views.py
+++ backend/addresses/views.py
@@ -15,6 +15,18 @@ class AddressListCreateView(generics.ListCreateAPIView):
         )
 
     def perform_create(self,serializer):
+
+        is_default = serializer.validated_date.get(
+            "is_dafault",
+            False
+        )
+
+        if is_default:
+            Address.objects.filter(
+                user = self.request.user,
+                is_default=True
+            ).update(is_default=False)
+
         serializer.save(user=self.request.user)
 
 
@@ -28,4 +40,20 @@ class AddressDetailView(generics.RetrieveUpdateDestroyAPIView):
             user=self.request.user
         )
 
+    def perform_update(self,serializer):
+        is_default = serializer.validated_data.get(
+            "is_default",
+            False
+        )
+
+        if is_default:
+            Address.objects.filter(
+                user = self.request.user,
+                is_default=True
+            ).exclude(
+                id=self.get_object().id
+            ).update(is_default=False)
+        serializer.save()
+
+        
     
